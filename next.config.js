621542ff69610073f2f73464c82e34fd9622bfdb
“katiewangmt” <katiewang.mt@gmail.com>
2025-01-19 23:08:28 -0800

    trying to fix

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [],
-    unoptimized: true,
-  },
+  reactStrictMode: true,
+  swcMinify: true,
 }
 
 module.exports = nextConfig 
